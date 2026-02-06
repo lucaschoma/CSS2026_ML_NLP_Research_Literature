38e866b91e89dbef5e1de41bef3c7a158ae9e96c
Lucas Choma (Semakaleng) <18102316+lucaschoma@users.noreply.github.com>
2026-02-06 18:05:34 +0000

    Commented feedparser import

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ from dataclasses import dataclass
 from datetime import datetime, timedelta, timezone
 from typing import Dict, List, Optional, Tuple
 
-import feedparser
+#import feedparser
 import pandas as pd
 import streamlit as st
 from bs4 import BeautifulSoup
